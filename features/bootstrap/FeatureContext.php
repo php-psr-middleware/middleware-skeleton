0bca64b7972ae635c5323104acf68eb594a846b2
Praveen Saxena <saxep01@gmail.com>
2018-03-14 07:49:00 -0500

    init repo

---
+++ features/bootstrap/FeatureContext.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types = 1);
+
+use Behat\Behat\Tester\Exception\PendingException;
+use Behat\Behat\Context\Context;
+use PHPUnit\Framework\Assert;
+
+class FeatureContext implements Context
+{
+
+}
